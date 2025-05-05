bd1edb6ad1b7295c0193489f0a3e49b975f0c822
DuaDepTrai <hieun.mgc@gmail.com>
2025-05-05 16:33:21 +0700

    update

---
+++ 250503/HRM8/HRM8/Controllers/ORGANIZATIONsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM8;
+
+namespace HRM8.Controllers
+{
+    public class ORGANIZATIONsController : Controller
+    {
+        private HRMDBEntities db = new HRMDBEntities();
+
+        // GET: ORGANIZATIONs
+        public ActionResult Index()
+        {
+            var list = db.ORGANIZATION.ToList();
+
+            foreach (var item in list) 
+            {
+                item.Parent = db.ORGANIZATION.FirstOrDefault(p => p.OrganizationID == item.ParentID);
+            }
+
+            return View(list);
+        }
+
+        // GET: ORGANIZATIONs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ORGANIZATION oRGANIZATION = db.ORGANIZATION.Find(id);
+            if (oRGANIZATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oRGANIZATION);
+        }
+
+        // GET: ORGANIZATIONs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ORGANIZATIONs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrganizationID,OrganizationName,Code,Address,Phone,Fax,Email,Website,ParentID,Lever")] ORGANIZATION oRGANIZATION)
+        {
+            if (ModelState.IsValid)
+            {
+                if (db.ORGANIZATION.Any(o => o.OrganizationName == oRGANIZATION.OrganizationName)) 
+                {
+                    ModelState.AddModelError("OrganizationName", "Organization Name is already used");
+                    return View(oRGANIZATION);
+                }
+                if (db.ORGANIZATION.Any(o => o.Code == oRGANIZATION.Code))
+                {
+                    ModelState.AddModelError("Code", "Organization Code is already used");
+                    return View(oRGANIZATION);
+                }
+
+                db.ORGANIZATION.Add(oRGANIZATION);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(oRGANIZATION);
+        }
+
+        // GET: ORGANIZATIONs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ORGANIZATION oRGANIZATION = db.ORGANIZATION.Find(id);
+            if (oRGANIZATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oRGANIZATION);
+        }
+
+        // POST: ORGANIZATIONs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrganizationID,OrganizationName,Code,Address,Phone,Fax,Email,Website,ParentID,Lever")] ORGANIZATION oRGANIZATION)
+        {
+            if (ModelState.IsValid)
+            {
+                if (db.ORGANIZATION.Any(o => o.OrganizationName == oRGANIZATION.OrganizationName && o.OrganizationID != oRGANIZATION.OrganizationID))
+                {
+                    ModelState.AddModelError("OrganizationName", "Organization Name is already used");
+                    return View(oRGANIZATION);
+                }
+                if (db.ORGANIZATION.Any(o => o.Code == oRGANIZATION.Code && o.OrganizationID != oRGANIZATION.OrganizationID))
+                {
+                    ModelState.AddModelError("Code", "Organization Code is already used");
+                    return View(oRGANIZATION);
+                }
+
+                db.Entry(oRGANIZATION).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(oRGANIZATION);
+        }
+
+        // GET: ORGANIZATIONs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ORGANIZATION oRGANIZATION = db.ORGANIZATION.Find(id);
+            if (oRGANIZATION == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oRGANIZATION);
+        }
+
+        // POST: ORGANIZATIONs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ORGANIZATION oRGANIZATION = db.ORGANIZATION.Find(id);
+
+            if (db.EMPLOYEE.Any(e => e.OrganizationID == oRGANIZATION.OrganizationID)) 
+            {
+                ModelState.AddModelError("", "Organization is being used, cannot remove");
+                return View(oRGANIZATION);
+            }
+
+            db.ORGANIZATION.Remove(oRGANIZATION);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 250503/HRM8/HRM8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HRM8.MvcApplication" Language="C#" %>
